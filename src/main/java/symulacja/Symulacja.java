18595523418bad7d83f86b454de89575a91ebab3
Antoni Fritz <antonifritz2000@gmail.com>
2020-05-04 20:49:36 +0200

    default

--- src/main/java/symulacja/Symulacja.java
+++ src/main/java/symulacja/Symulacja.java
@@ -5,36 +5,23 @@ import jednostka.Dorosly;
 import jednostka.Dziecko;
 import jednostka.Jednostka;
 import jednostka.Zwierze;
+import pole.Skaly;
+
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 
 public class Symulacja {
+    private Mapa mapa;
+
+    public Symulacja(Mapa mapa)
+    {
+        this.mapa=mapa;
+    }
     public static void main(String[] args)
     {
-        List<Jednostka>listaJednostek=new LinkedList<>();
         Mapa mapa = new Mapa();
-        mapa.losujkonfiguracje(mapa.tablica);
-        mapa.rozmiescPola(mapa.tablicaPol);
-        Dorosly mezczyzna = new Dorosly(mapa,100,100,0, mapa.tablicaPol[0][0],"Zbigniew");
-        Dorosly kobieta = new Dorosly(mapa,100,100,0, mapa.tablicaPol[0][0],"Katarzyna");
-        Dziecko dziecko = new Dziecko(mapa,100,100,0, mapa.tablicaPol[0][0],"Ludwik");
-        Zwierze owca = new Zwierze(mapa,100,100,0 , mapa.tablicaPol[0][0],"Beczka");
-        Zwierze swinia = new Zwierze(mapa,100,100,0, mapa.tablicaPol[0][0],"Chrumkas");
-        listaJednostek.add(mezczyzna);
-        listaJednostek.add(kobieta);
-        listaJednostek.add(dziecko);
-        listaJednostek.add(owca);
-        listaJednostek.add(swinia);
-        while (listaJednostek.size()>=1)
-        {
-            for (Jednostka jednostka :listaJednostek) {
-               jednostka.pobierzNowaLokalizacje(mapa.tablicaPol,10,10);
-            }
-        }
-        System.out.println("ZBIJEW TO ZIOMAL");
-        for (int i=0;i<mapa.dlugosc;i++)
-        { for(int j=0;j<mapa.szerokosc;j++)
-                System.out.println("tablica["+i+"]["+j+"]="+mapa.tablica[i][j]);}
+        Symulacja symulacja = new Symulacja(mapa);
     }
 }
