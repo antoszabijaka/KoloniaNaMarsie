f3de18780918342a0d4473120ca92e22bd803969
Antoni Fritz <antonifritz2000@gmail.com>
2020-05-04 18:25:08 +0200

    default

--- src/main/java/jednostka/Jednostka.java
+++ src/main/java/jednostka/Jednostka.java
@@ -52,7 +52,4 @@ public abstract class Jednostka {
         return tablicaPol[i][j];
     }
 
-
-
-
 }
--- src/main/java/symulacja/Symulacja.java
+++ src/main/java/symulacja/Symulacja.java
@@ -8,11 +8,14 @@ import jednostka.Zwierze;
 import pole.*;
 import pole.Pole;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 public class Symulacja {
     public static void main(String[] args)
     {
+        List<Jednostka>lista=new LinkedList<>();
         Mapa mapa = new Mapa();
         mapa.losujkonfiguracje(mapa.tablica);
         mapa.rozmiescPola(mapa.tablicaPol);
@@ -21,11 +24,15 @@ public class Symulacja {
         Dziecko dziecko = new Dziecko(mapa,100,100,0, mapa.tablicaPol[0][0],"Ludwik");
         Zwierze owca = new Zwierze(mapa,100,100,0 , mapa.tablicaPol[0][0],"Beczka");
         Zwierze swinia = new Zwierze(mapa,100,100,0, mapa.tablicaPol[0][0],"Chrumkas");
-
+        lista.add(mezczyzna);
+        lista.add(kobieta);
+        lista.add(dziecko);
+        lista.add(owca);
+        lista.add(swinia);
         System.out.println("ZBIJEW TO ZIOMAL");
         for (int i=0;i<mapa.dlugosc;i++)
         { for(int j=0;j<mapa.szerokosc;j++)
                 System.out.println("tablica["+i+"]["+j+"]="+mapa.tablica[i][j]);}
-
+        mezczyzna.nowaLokalizacja=mezczyzna.pobierzNowaLokalizacje(mapa.tablicaPol,10,10);
     }
 }
