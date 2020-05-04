ff407269a4a5eb86787c674708dba50fe1853f39
Antoni Fritz <antonifritz2000@gmail.com>
2020-05-04 23:52:20 +0200

    dodane interfejsy

--- src/main/java/symulacja/Symulacja.java
+++ src/main/java/symulacja/Symulacja.java
@@ -5,16 +5,31 @@ import jednostka.Dorosly;
 import jednostka.Dziecko;
 import jednostka.Jednostka;
 import jednostka.Zwierze;
-import pole.Skaly;
-
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
+
 
 
 public class Symulacja {
     private Mapa mapa;
+    private List<Jednostka>listaJednostek=new LinkedList<>();
 
+    public void startSymulacji(List<Jednostka>listaJednostek)
+    {
+        Dorosly mezczyzna = new Dorosly(mapa,100,100,0, mapa.tablicaPol[0][0],"Zbigniew");
+        Dorosly kobieta = new Dorosly(mapa,100,100,0, mapa.tablicaPol[0][0],"Katarzyna");
+        Dziecko dziecko = new Dziecko(mapa,100,100,0, mapa.tablicaPol[0][0],"Ludwik");
+        Zwierze owca = new Zwierze(mapa,100,100,0 , mapa.tablicaPol[0][0],"Beczka");
+        Zwierze swinia = new Zwierze(mapa,100,100,0, mapa.tablicaPol[0][0],"Chrumkas");
+        listaJednostek.add(mezczyzna);
+        listaJednostek.add(kobieta);
+        listaJednostek.add(dziecko);
+        listaJednostek.add(owca);
+        listaJednostek.add(swinia);
+        for (Jednostka jednostka:listaJednostek) {
+            jednostka.nowaLokalizacja=jednostka.pobierzNowaLokalizacje(mapa.tablicaPol,mapa.dlugosc,mapa.szerokosc);
+        }
+    }
     public Symulacja(Mapa mapa)
     {
         this.mapa=mapa;
@@ -22,6 +37,9 @@ public class Symulacja {
     public static void main(String[] args)
     {
         Mapa mapa = new Mapa();
+        mapa.losujkofiguracje(mapa.tablica);
+        mapa.rozmiescpola(mapa.tablicaPol);
         Symulacja symulacja = new Symulacja(mapa);
+        symulacja.startSymulacji(symulacja.listaJednostek);
     }
 }
