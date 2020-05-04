2b65d369aef4fbe5ecb1801bdf93c03115a67193
Antoni Fritz <antonifritz2000@gmail.com>
2020-05-05 00:39:09 +0200

    coś tam

--- src/main/java/symulacja/Symulacja.java
+++ src/main/java/symulacja/Symulacja.java
@@ -5,6 +5,8 @@ import jednostka.Dorosly;
 import jednostka.Dziecko;
 import jednostka.Jednostka;
 import jednostka.Zwierze;
+import pole.Skaly;
+
 import java.util.LinkedList;
 import java.util.List;
 
@@ -16,6 +18,8 @@ public class Symulacja {
 
     public void startSymulacji(List<Jednostka>listaJednostek)
     {
+        mapa.losujkofiguracje(mapa.tablica);
+        mapa.rozmiescpola(mapa.tablicaPol);
         Dorosly mezczyzna = new Dorosly(mapa,100,100,0, mapa.tablicaPol[0][0],"Zbigniew");
         Dorosly kobieta = new Dorosly(mapa,100,100,0, mapa.tablicaPol[0][0],"Katarzyna");
         Dziecko dziecko = new Dziecko(mapa,100,100,0, mapa.tablicaPol[0][0],"Ludwik");
@@ -26,6 +30,10 @@ public class Symulacja {
         listaJednostek.add(dziecko);
         listaJednostek.add(owca);
         listaJednostek.add(swinia);
+        tura();
+    }
+    public void tura()
+    {   while(listaJednostek.size()>=1)
         for (Jednostka jednostka:listaJednostek) {
             jednostka.nowaLokalizacja=jednostka.pobierzNowaLokalizacje(mapa.tablicaPol,mapa.dlugosc,mapa.szerokosc);
         }
@@ -37,8 +45,6 @@ public class Symulacja {
     public static void main(String[] args)
     {
         Mapa mapa = new Mapa();
-        mapa.losujkofiguracje(mapa.tablica);
-        mapa.rozmiescpola(mapa.tablicaPol);
         Symulacja symulacja = new Symulacja(mapa);
         symulacja.startSymulacji(symulacja.listaJednostek);
     }
